9cf0bc45b13499a8cdcd8483ea6a3a8b57758737
bigcup <scr3am1@gmail.com>
2015-09-24 21:39:40 +0300

    working provision with global npm modules

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,16 +1,21 @@
+#!/usr/bin/env bash
 
+echo "updating"
 apt-get update
+
+echo "install node"
 curl --silent --location https://deb.nodesource.com/setup_4.x | sudo bash -
 apt-get install --yes nodejs
-apt-get install --yes --silent icecast2 liquidsoap
+apt-get install --yes --silent liquidsoap
 
+echo "install npm tools"
+apt-get install --yes build-essential
+
+echo "configure and update npm"
 npm install npm -g
-mkdir ~/npm-global
-npm config set prefix '~/npm-global'
-echo 'export PATH=~/npm-global/bin:$PATH' >>  ~/.profile
-source ~/.profile
-# npm config set prefix ~/npm
-# npm install -g npm
+mkdir /home/npm-global
+sudo NPM_CONFIG_PREFIX=/home/npm-global npm install gulp node-gyp nodemon forever -g --no-bin-links
+echo "finished"
 # npm install -g --no-bin-links gulp nodemon forever node-gyp
 # PATH="$PATH:$HOME/npm/bin"
 # cd /vagrant/web
