2154f2ae7d612ae8374a3718d87adce615508f6b
bigcup <scr3am1@gmail.com>
2015-10-25 19:15:17 +0200

    markdown fix

--- web/server/routes/posters.js
+++ web/server/routes/posters.js
@@ -3,11 +3,34 @@ var router = express.Router();
 // md compiler
 var marked = require('marked');
 // default values
+var renderer = new marked.Renderer();
+
+renderer.link = function(href, title, text) {
+  if (this.options.sanitize) {
+    try {
+      var prot = decodeURIComponent(unescape(href))
+        .replace(/[^\w:]/g, '')
+        .toLowerCase();
+    } catch (e) {
+      return '';
+    }
+    if (prot.indexOf('javascript:') === 0 || prot.indexOf('vbscript:') === 0) {
+      return '';
+    }
+  }
+  var out = '<a target="_blank" href="' + href + '"';
+  if (title) {
+    out += ' title="' + title + '"';
+  }
+  out += '>' + text + '</a>';
+  return out;
+};
+
 marked.setOptions({
   renderer: new marked.Renderer(),
   gfm: true,
   tables: true,
-  breaks: false,
+  breaks: true,
   pedantic: false,
   sanitize: true,
   smartLists: true,
