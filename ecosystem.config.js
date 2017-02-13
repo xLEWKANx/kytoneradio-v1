c4ece55e5be8b963ddd037cc6d49cc382301bdd9
bigcup <scr3am1@gmail.com>
2017-02-13 03:45:24 +0200

    ecosystem fix

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -30,7 +30,7 @@ module.exports = {
       ref: "origin/master",
       repo: "git@github.com:bigcup/kytoneradio.git",
       path: "/var/www/production",
-      "post-deploy": "npm install && pm2 startOrRestart ecosystem.json --env production"
+      "post-deploy": "cd web || npm install && pm2 startOrRestart ecosystem.json --env production"
     },
     dev: {
       user: "curator",
@@ -39,7 +39,7 @@ module.exports = {
       ref: "origin/loopback-integration",
       repo: "git@github.com:bigcup/kytoneradio.git",
       path: "/var/www/development",
-      "post-deploy": "npm install && pm2 startOrRestart ecosystem.json --env dev",
+      "post-deploy": "cd web || npm install && pm2 startOrRestart ecosystem.json --env dev",
       env: {
         NODE_ENV: "dev"
       }
